baac2168d7df1baefade6174124023d029167db4
santiagojc <santijc92@gmail.com>
2021-09-17 12:07:29 -0500

    Se termina proyecto de convocatoria con clase Jugador, su respectivo menu, agregar jugador, buscar por nro camiseta, editar jugador y toda la nomina de jugadores

--- src/convocatorias/Convocatorias.java
+++ src/convocatorias/Convocatorias.java
@@ -34,7 +34,7 @@ public class Convocatorias {
             System.out.println("***************");
             
             System.out.println("1. Agregar jugador");
-            System.out.println("2. Buscar jugador");
+            System.out.println("2. Buscar jugador por nro camiseta");
             System.out.println("3. Editar informacion de jugador");
             System.out.println("4. Mostrar todos los jugadores");
             System.out.println("5. Salir");
@@ -44,12 +44,13 @@ public class Convocatorias {
             switch(opc){
             
                 case 1:
+                    
                     System.out.println("Registro de jugadores");
                     System.out.println("");
                     jugadores[cont]=new Jugador();
                     /*jugadores.set(cont, new Jugador());*/
                     
-                    
+                    if(cont <=23){
                     
                     System.out.println("Digite el numero de la camiseta");
                     jugadores[cont].setNumeroCamiseta(entrada.nextByte());
@@ -77,25 +78,84 @@ public class Convocatorias {
                     /*jugadores.get(cont).setEquipoOrigen(entrada.next());*/
                     
                     cont=cont+1;
-                    
+                    }else{
+                        System.out.println("No se pueden agregar mas jugadores");
+                    }
                     
                     
                     
                      break;
                 case 2:
+                    int buscar;
+                    System.out.println("Ingrese el nro que camiseta a buscar");
+                    buscar=entrada.nextInt();
+                    for(int i=0;i<cont;i++){
+                        if(buscar==jugadores[i].getNumeroCamiseta()){
+                        
+                            System.out.println("Nombre del jugador " + jugadores[i].getNombre());
+                            System.out.println("Apellidos del jugador " + jugadores[i].getApellidos());
+                            System.out.println("Posicion en la que juga " + jugadores[i].getPosicion());
+                            System.out.println("Edad del jugador " + jugadores[i].getEdad());
+                            System.out.println("Equipo donde juga " + jugadores[i].getEquipoOrigen());
+                            System.out.println("");
+                        
+                        }else{
+                        
+                            System.out.println("El jugador no fue convocado");
+                        
+                        }
+                        
+                     }
+               
+                        
+                    
+                    
                     break;
                 case 3:
+                    
+                    System.out.println("Ingrese el nro de camiseta del jugador a editar");
+                    buscar=entrada.nextInt();
+                    for(int i=0;i<cont;i++){
+                        if(buscar==jugadores[i].getNumeroCamiseta()){
+                        
+                            System.out.println("Edite el nro de la camisa del jugador");
+                            jugadores[i].setNumeroCamiseta(entrada.nextByte());
+                            System.out.println("Nombre del jugador " + jugadores[i].getNombre());
+                            System.out.println("Edite el nombre del jugador");
+                            jugadores[i].setNombre(entrada.next());
+                            System.out.println("Apellidos del jugador " + jugadores[i].getApellidos());
+                            System.out.println("Edite el apellido del jugador");
+                            jugadores[i].setApellidos(entrada.next());
+                            System.out.println("Posicion en la que juga " + jugadores[i].getPosicion());
+                            System.out.println("Edite la posicion del jugador");
+                            jugadores[i].setPosicion(entrada.next());
+                            System.out.println("Edad del jugador " + jugadores[i].getEdad());
+                            System.out.println("Edite la edad del jugador");
+                            jugadores[i].setEdad(entrada.nextByte());
+                            System.out.println("Equipo donde juga " + jugadores[i].getEquipoOrigen());
+                            System.out.println("Edite el equipo origen del jugador");
+                            jugadores[i].setEquipoOrigen(entrada.next());
+                            System.out.println("");
+                        
+                        }else{
+                        
+                            System.out.println("El jugador no ha sido registrado");
+                        
+                        }
+                        
+                     }
+                    
                     break;
                 case 4:
                     
                     for(int i=0; i<cont;i++){
                     
-                        System.out.println(jugadores[i].getNumeroCamiseta());
-                        System.out.println(jugadores[i].getNombre());
-                        System.out.println(jugadores[i].getApellidos());
-                        System.out.println(jugadores[i].getPosicion());
-                        System.out.println(jugadores[i].getEdad());
-                        System.out.println(jugadores[i].getEquipoOrigen());
+                        System.out.println("Numero de camiseta es " + jugadores[i].getNumeroCamiseta());
+                        System.out.println("Nombre del jugador " + jugadores[i].getNombre());
+                        System.out.println("Apellidos del jugador " + jugadores[i].getApellidos());
+                        System.out.println("Posicion en la que juga " + jugadores[i].getPosicion());
+                        System.out.println("Edad del jugador " + jugadores[i].getEdad());
+                        System.out.println("Equipo donde juga " + jugadores[i].getEquipoOrigen());
                         System.out.println("");
                     
                     }
--- src/jugador/Jugador.java
+++ src/jugador/Jugador.java
@@ -82,6 +82,8 @@ public class Jugador {
     public void setEquipoOrigen(String equipoOrigen) {
         this.equipoOrigen = equipoOrigen;
     }
+
+    
     
     
 
